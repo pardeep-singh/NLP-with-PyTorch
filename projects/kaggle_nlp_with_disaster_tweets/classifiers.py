8aba90a28672e0c56d8315189af9aef690ff76d4
Pardeep Singh <pardeep@sensehq.com>
2021-07-04 12:51:23 +0530

    Practice Project: Kaggle NLP with Disaster Tweets
    - Add MLP classifier with one more hidden layer.

--- projects/kaggle_nlp_with_disaster_tweets/classifiers.py
+++ projects/kaggle_nlp_with_disaster_tweets/classifiers.py
@@ -60,3 +60,38 @@ class TweetMLPClassifier(nn.Module):
         if apply_sigmoid:
             prediction_vector = F.sigmoid(prediction_vector)
         return prediction_vector
+
+
+class TweetMLPClassifier1(nn.Module):
+    """
+    A 3-layer Multilayer Perceptron classifying Tweets.
+    """
+
+    def __init__(self, input_dim, hidden_dim1, hidden_dim2, output_dim):
+        """
+        :param input_dim: the size of the input vectors.
+        :param hidden_dim1: the output size of the first Linear Layer.
+        :param hidden_dim2: the output size of the second Linear Layer.
+        :param output_dim: the output size of the second Linear Layer.
+        """
+        super(TweetMLPClassifier1, self).__init__()
+        self.fc1 = nn.Linear(input_dim, hidden_dim1)
+        self.fc2 = nn.Linear(hidden_dim1, hidden_dim2)
+        self.fc3 = nn.Linear(hidden_dim2, output_dim)
+
+    def forward(self, x_in, apply_sigmoid=False):
+        """
+        The forward pass of the classifier.
+
+        :param x_in: an input data tensor.
+            X_in.shape should be (batch, input_dim).
+        :param apply_sigmoid: a flag for the sigmoid activation.
+            It should be false if used with the cross-entropy losses.
+        :return: the resulting tensor. Shape should be (batch, output_dim).
+        """
+        intermediate_vector1 = F.relu(self.fc1(x_in))
+        intermediate_vector2 = F.relu(self.fc2(intermediate_vector1))
+        prediction_vector = self.fc3(intermediate_vector2).squeeze()
+        if apply_sigmoid:
+            prediction_vector = F.sigmoid(prediction_vector)
+        return prediction_vector
