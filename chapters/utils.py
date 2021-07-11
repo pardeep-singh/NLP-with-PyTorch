b5eb8368d53d798e247de044868c51352b78e335
Pardeep Singh <pardeep@sensehq.com>
2021-07-11 16:02:26 +0530

    Chapter 05: Embedding Words and Types
    - Complete section 2 and start section implementation.

---
+++ chapters/utils.py
@@ -0,0 +1,280 @@
+from torch.utils.data import DataLoader
+import torch
+import numpy as np
+import os
+from tqdm import notebook
+import torch.nn.functional as F
+
+
+def generate_batches(dataset, batch_size, shuffle=True, drop_last=True, device="cpu"):
+    """
+    Function to wrap PyTorch DataLoader. Ensures that each tensor
+    is on the write device location.
+
+    :param dataset: Dataset Object.
+    :param batch_size: value to split the dataset into smaller batches.
+    :param shuffle: Boolean param to control the shuffle behaviour while
+        splitting the data. Defaults to True
+    :param drop_last: Weather to drop last item or not. Defaults to True
+    :param device: Device to load the tensor to. default to CPU
+    :return: Iterator object
+    """
+    dataloader = DataLoader(
+        dataset=dataset, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last
+    )
+    for data_dict in dataloader:
+        out_data_dict = {}
+        for name, tensor in data_dict.items():
+            out_data_dict[name] = data_dict[name].to(device)
+        yield out_data_dict
+
+
+# Helpers
+def set_seed_everywhere(seed, cuda):
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if cuda:
+        torch.cuda.manual_seed_all(seed)
+
+
+def handle_dirs(dirpath):
+    if not os.path.exists(dirpath):
+        os.makedirs(dirpath)
+
+
+def make_train_state(args):
+    return {
+        "stop_early": False,
+        "early_stopping_step": 0,
+        "early_stopping_best_val": 1e8,
+        "learning_rate": args.learning_rate,
+        "epoch_index": 0,
+        "train_loss": [],
+        "train_acc": [],
+        "val_loss": [],
+        "val_acc": [],
+        "test_loss": -1,
+        "test_acc": -1,
+        "model_filename": args.model_state_file,
+    }
+
+
+def compute_accuracy(y_pred, y_target):
+    _, y_pred_indices = _, y_pred_indices = y_pred.max(dim=1)
+    n_correct = torch.eq(y_pred_indices, y_target).sum().item()
+    return n_correct / len(y_pred_indices) * 100
+
+
+def update_train_state(args, model, train_state):
+    """Handle the training state updates.
+
+    Components:
+     - Early Stopping: Prevent overfitting.
+     - Model Checkpoint: Model is saved if the model is better
+
+    :param args: main arguments
+    :param model: model to train
+    :param train_state: a dictionary representing the training state values
+    :returns:
+        a new train_state
+    """
+    # Save one model at least
+    if train_state["epoch_index"] == 0:
+        torch.save(model.state_dict(), train_state["model_filename"])
+        train_state["stop_early"] = False
+
+    # Save model if performance improved
+    elif train_state["epoch_index"] >= 1:
+        loss_tm1, loss_t = train_state["val_loss"][-2:]
+
+        # If loss worsened
+        if loss_t >= train_state["early_stopping_best_val"]:
+            # Update step
+            train_state["early_stopping_step"] += 1
+        # Loss decreased
+        else:
+            # Save the best model
+            if loss_t < train_state["early_stopping_best_val"]:
+                torch.save(model.state_dict(), train_state["model_filename"])
+
+            # Reset early stopping step
+            train_state["early_stopping_step"] = 0
+
+        # Stop early ?
+        train_state["stop_early"] = (
+            train_state["early_stopping_step"] >= args.early_stopping_criteria
+        )
+
+    return train_state
+
+
+def train_model(classifier, loss_func, optimizer, scheduler, dataset, args):
+    classifier = classifier.to(args.device)
+    train_state = make_train_state(args)
+    epoch_bar = notebook.tqdm(
+        desc="Training Routine", total=args.num_epochs, position=0
+    )
+    dataset.set_split("train")
+    train_bar = notebook.tqdm(
+        desc="split=train",
+        total=dataset.get_num_batches(args.batch_size),
+        position=1,
+        leave=True,
+    )
+    dataset.set_split("val")
+    val_bar = notebook.tqdm(
+        desc="split=val",
+        total=dataset.get_num_batches(args.batch_size),
+        position=1,
+        leave=True,
+    )
+
+    for epoch_index in range(args.num_epochs):
+        train_state["epoch_index"] = epoch_index
+        # Iterate Over Training Dataset
+        # Setup: Batch Generator, set loss & acc to 0, set train mode on
+        dataset.set_split("train")
+        if epoch_index == 0:
+            print(
+                f"============ Split={dataset._train_split}, Size={len(dataset)} ============"
+            )
+        batch_generator = generate_batches(
+            dataset, batch_size=args.batch_size, device=args.device
+        )
+        training_running_loss, training_running_acc = 0.0, 0.0
+        classifier.train()
+
+        for batch_index, batch_dict in enumerate(batch_generator):
+            # 5 Step Training Routine
+
+            # Step 1. Zero the Gradients
+            optimizer.zero_grad()
+
+            # Step 2. Compute the gradients
+            y_pred = classifier(x_in=batch_dict["x_data"])
+
+            # Step 3. Compute the Output
+            loss = loss_func(y_pred, batch_dict["y_target"])
+            loss_batch = loss.item()
+            training_running_loss += (loss_batch - training_running_loss) / (
+                batch_index + 1
+            )
+
+            # Step 4. Use loss to produce gradients
+            loss.backward()
+
+            # Step 5. Use Optimizer to take gradient step
+            optimizer.step()
+
+            # Compute the accuracy
+            acc_batch = compute_accuracy(y_pred, batch_dict["y_target"])
+            training_running_acc += (acc_batch - training_running_acc) / (
+                batch_index + 1
+            )
+
+            # Update the bar
+            train_bar.set_postfix(
+                loss=training_running_loss, acc=training_running_acc, epoch=epoch_index
+            )
+            train_bar.update()
+        train_state["train_loss"].append(training_running_loss)
+        train_state["train_acc"].append(training_running_acc)
+
+        # Iterate Over Val Dataset
+        # Setup: Batch Generator, set loss and acc to 0, set eval mode on
+        dataset.set_split("val")
+        val_running_loss, val_running_acc = 0.0, 0.0
+        if len(dataset) > 0:
+            if epoch_index == 0:
+                print(
+                    f"============ Split={dataset._train_split}, Size={len(dataset)} ============"
+                )
+            batch_generator = generate_batches(
+                dataset, batch_size=args.batch_size, device=args.device
+            )
+            classifier.eval()
+
+            for batch_index, batch_dict in enumerate(batch_generator):
+                # Step 1. Compute the Output
+                y_pred = classifier(x_in=batch_dict["x_data"])
+
+                # Step 2. Compute the loss
+                loss = loss_func(y_pred, batch_dict["y_target"])
+                loss_batch = loss.item()
+                val_running_loss += (loss_batch - val_running_loss) / (batch_index + 1)
+
+                # Step 3. Compute the accuracy
+                acc_batch = compute_accuracy(y_pred, batch_dict["y_target"])
+                val_running_acc += (acc_batch - val_running_acc) / (batch_index + 1)
+                val_bar.set_postfix(
+                    loss=val_running_loss, acc=val_running_acc, epoch=epoch_index
+                )
+                val_bar.update()
+            train_state["val_loss"].append(val_running_loss)
+            train_state["val_acc"].append(val_running_acc)
+            scheduler.step(train_state["val_loss"][-1])
+        else:
+            if epoch_index == 0:
+                print(f"============ Skipping Validation Pass ============")
+            train_state["val_loss"].append(val_running_loss)
+            train_state["val_acc"].append(val_running_acc)
+            scheduler.step(train_state["train_loss"][-1])
+        train_state = update_train_state(
+            args=args, model=classifier, train_state=train_state
+        )
+
+        train_bar.n, val_bar.n = 0, 0
+        epoch_bar.update()
+
+        if train_state["stop_early"]:
+            print("Stopping early....")
+            break
+
+        if epoch_index % 10 == 0:
+            print(
+                f"--------------- {epoch_index}th Epoch Stats---------------\n"
+                f"Training Loss={training_running_loss}, "
+                f"Training Accuracy={training_running_acc}\n"
+                f"Validation Loss={val_running_loss}, "
+                f"Validation Accuracy={val_running_acc}.\n"
+                "------------------------------------------------------------"
+            )
+    return train_state
+
+
+def evaluate_test_split(classifier, dataset, loss_func, train_state, args):
+    classifier = classifier.to(args.device)
+    dataset.set_split("test")
+    print(
+        f"============ Split={dataset._train_split}, Size={len(dataset)} ============"
+    )
+    running_loss, running_acc = 0.0, 0.0
+    if len(dataset) == 0:
+        train_state["test_loss"] = running_loss
+        train_state["test_acc"] = running_acc
+        return train_state
+    batch_generator = generate_batches(
+        dataset, batch_size=args.batch_size, device=args.device
+    )
+    classifier.eval()
+
+    for batch_index, batch_dict in enumerate(batch_generator):
+        # Step 1. Compute the Output
+        y_pred = classifier(x_in=batch_dict["x_data"])
+
+        # Step 2. Compute the loss
+        loss = loss_func(y_pred, batch_dict["y_target"])
+        loss_batch = loss.item()
+        running_loss += (loss_batch - running_loss) / (batch_index + 1)
+
+        # Step 3. Compute the accuracy
+        acc_batch = compute_accuracy(y_pred, batch_dict["y_target"])
+        running_acc += (acc_batch - running_acc) / (batch_index + 1)
+
+    train_state["test_loss"] = running_loss
+    train_state["test_acc"] = running_acc
+    print(f"-------- Test Accuracy={running_acc}, Test Loss={running_loss}.--------")
+    train_state = update_train_state(
+        args=args, model=classifier, train_state=train_state
+    )
+    return train_state
